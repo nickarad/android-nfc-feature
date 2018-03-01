241798d798c6a76497db12821ca048c92b355829
nickarad <nickarad94@gmail.com>
2018-03-01 15:55:47 +0200

    Disable Useles Code by Commenting

--- app/src/main/java/com/example/nikos/nfc/MainActivity.java
+++ app/src/main/java/com/example/nikos/nfc/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewInfo = (TextView)findViewById(R.id.info);
-        textViewTagInfo = (TextView)findViewById(R.id.taginfo);
+//        textViewTagInfo = (TextView)findViewById(R.id.taginfo);
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if(nfcAdapter == null){
@@ -55,29 +55,30 @@ public class MainActivity extends AppCompatActivity {
 
         if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
             Toast.makeText(this,
-                    "onResume() - ACTION_TECH_DISCOVERED",
+                    "MifareClassic Detected",
                     Toast.LENGTH_SHORT).show();
 
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             if(tag == null){
                 textViewInfo.setText("tag == null");
             }else{
-                String tagInfo = tag.toString() + "\n";
+                String tagInfo = "\n";
 
-                tagInfo += "\nTag Id: \n";
+//                tagInfo += "\nTag Id: \n";
                 byte[] tagId = tag.getId();
-                tagInfo += "length = " + tagId.length +"\n";
+//                tagInfo += "length = " + tagId.length +"\n";
+            //----------Display Tag ID --------------------
                 for(int i=0; i<tagId.length; i++){
                     tagInfo += Integer.toHexString(tagId[i] & 0xFF) + " ";
                 }
                 tagInfo += "\n";
-
-                String[] techList = tag.getTechList();
-                tagInfo += "\nTech List\n";
-                tagInfo += "length = " + techList.length +"\n";
-                for(int i=0; i<techList.length; i++){
-                    tagInfo += techList[i] + "\n ";
-                }
+//
+//                String[] techList = tag.getTechList();
+//                tagInfo += "\nTech List\n";
+//                tagInfo += "length = " + techList.length +"\n";
+//                for(int i=0; i<techList.length; i++){
+//                    tagInfo += techList[i] + "\n ";
+//                }
 
                 textViewInfo.setText(tagInfo);
 
@@ -97,47 +98,47 @@ public class MainActivity extends AppCompatActivity {
 
         String typeInfoString = "--- MifareClassic tag ---\n";
         int type = mifareClassicTag.getType();
-        switch(type){
-            case MifareClassic.TYPE_PLUS:
-                typeInfoString += "MifareClassic.TYPE_PLUS\n";
-                break;
-            case MifareClassic.TYPE_PRO:
-                typeInfoString += "MifareClassic.TYPE_PRO\n";
-                break;
-            case MifareClassic.TYPE_CLASSIC:
-                typeInfoString += "MifareClassic.TYPE_CLASSIC\n";
-                break;
-            case MifareClassic.TYPE_UNKNOWN:
-                typeInfoString += "MifareClassic.TYPE_UNKNOWN\n";
-                break;
-            default:
-                typeInfoString += "unknown...!\n";
-        }
+//        switch(type){
+//            case MifareClassic.TYPE_PLUS:
+//                typeInfoString += "MifareClassic.TYPE_PLUS\n";
+//                break;
+//            case MifareClassic.TYPE_PRO:
+//                typeInfoString += "MifareClassic.TYPE_PRO\n";
+//                break;
+//            case MifareClassic.TYPE_CLASSIC:
+//                typeInfoString += "MifareClassic.TYPE_CLASSIC\n";
+//                break;
+//            case MifareClassic.TYPE_UNKNOWN:
+//                typeInfoString += "MifareClassic.TYPE_UNKNOWN\n";
+//                break;
+//            default:
+//                typeInfoString += "unknown...!\n";
+//        }
 
-        int size = mifareClassicTag.getSize();
-        switch(size){
-            case MifareClassic.SIZE_1K:
-                typeInfoString += "MifareClassic.SIZE_1K\n";
-                break;
-            case MifareClassic.SIZE_2K:
-                typeInfoString += "MifareClassic.SIZE_2K\n";
-                break;
-            case MifareClassic.SIZE_4K:
-                typeInfoString += "MifareClassic.SIZE_4K\n";
-                break;
-            case MifareClassic.SIZE_MINI:
-                typeInfoString += "MifareClassic.SIZE_MINI\n";
-                break;
-            default:
-                typeInfoString += "unknown size...!\n";
-        }
-
-        int blockCount = mifareClassicTag.getBlockCount();
-        typeInfoString += "BlockCount \t= " + blockCount + "\n";
-        int sectorCount = mifareClassicTag.getSectorCount();
-        typeInfoString += "SectorCount \t= " + sectorCount + "\n";
-
-        textViewTagInfo.setText(typeInfoString);
+//        int size = mifareClassicTag.getSize();
+//        switch(size){
+//            case MifareClassic.SIZE_1K:
+//                typeInfoString += "MifareClassic.SIZE_1K\n";
+//                break;
+//            case MifareClassic.SIZE_2K:
+//                typeInfoString += "MifareClassic.SIZE_2K\n";
+//                break;
+//            case MifareClassic.SIZE_4K:
+//                typeInfoString += "MifareClassic.SIZE_4K\n";
+//                break;
+//            case MifareClassic.SIZE_MINI:
+//                typeInfoString += "MifareClassic.SIZE_MINI\n";
+//                break;
+//            default:
+//                typeInfoString += "unknown size...!\n";
+//        }
+//
+//        int blockCount = mifareClassicTag.getBlockCount();
+//        typeInfoString += "BlockCount \t= " + blockCount + "\n";
+//        int sectorCount = mifareClassicTag.getSectorCount();
+//        typeInfoString += "SectorCount \t= " + sectorCount + "\n";
+//
+//        textViewTagInfo.setText(typeInfoString);
     }
 
 
