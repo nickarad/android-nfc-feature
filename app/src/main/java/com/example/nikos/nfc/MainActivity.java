2b0894da78f9d9a4c4400f2dbb4a96404033d053
nickarad <nickarad94@gmail.com>
2018-03-06 17:17:53 +0200

    Delete useless comments

--- app/src/main/java/com/example/nikos/nfc/MainActivity.java
+++ app/src/main/java/com/example/nikos/nfc/MainActivity.java
@@ -41,11 +41,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
             finish();
         }
-
     }
-
-
-
 
     @Override
     protected void onResume() {
@@ -73,13 +69,6 @@ public class MainActivity extends AppCompatActivity {
                     tagInfo += Integer.toHexString(tagId[i] & 0xFF) + " ";
                 }
                 tagInfo += "\n";
-//
-//                String[] techList = tag.getTechList();
-//                tagInfo += "\nTech List\n";
-//                tagInfo += "length = " + techList.length +"\n";
-//                for(int i=0; i<techList.length; i++){
-//                    tagInfo += techList[i] + "\n ";
-//                }
 
                 textViewInfo.setText(tagInfo);
 
@@ -99,47 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
         String typeInfoString = "--- MifareClassic tag ---\n";
         int type = mifareClassicTag.getType();
-//        switch(type){
-//            case MifareClassic.TYPE_PLUS:
-//                typeInfoString += "MifareClassic.TYPE_PLUS\n";
-//                break;
-//            case MifareClassic.TYPE_PRO:
-//                typeInfoString += "MifareClassic.TYPE_PRO\n";
-//                break;
-//            case MifareClassic.TYPE_CLASSIC:
-//                typeInfoString += "MifareClassic.TYPE_CLASSIC\n";
-//                break;
-//            case MifareClassic.TYPE_UNKNOWN:
-//                typeInfoString += "MifareClassic.TYPE_UNKNOWN\n";
-//                break;
-//            default:
-//                typeInfoString += "unknown...!\n";
-//        }
 
-//        int size = mifareClassicTag.getSize();
-//        switch(size){
-//            case MifareClassic.SIZE_1K:
-//                typeInfoString += "MifareClassic.SIZE_1K\n";
-//                break;
-//            case MifareClassic.SIZE_2K:
-//                typeInfoString += "MifareClassic.SIZE_2K\n";
-//                break;
-//            case MifareClassic.SIZE_4K:
-//                typeInfoString += "MifareClassic.SIZE_4K\n";
-//                break;
-//            case MifareClassic.SIZE_MINI:
-//                typeInfoString += "MifareClassic.SIZE_MINI\n";
-//                break;
-//            default:
-//                typeInfoString += "unknown size...!\n";
-//        }
-//
-//        int blockCount = mifareClassicTag.getBlockCount();
-//        typeInfoString += "BlockCount \t= " + blockCount + "\n";
-//        int sectorCount = mifareClassicTag.getSectorCount();
-//        typeInfoString += "SectorCount \t= " + sectorCount + "\n";
-//
-//        textViewTagInfo.setText(typeInfoString);
     }
 
 
